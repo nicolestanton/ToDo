701cbcc5688d64085067b0d095d5c44d91e495f6
Nicole Stanton <n.stanton14@ymail.com>
2020-07-01 21:46:29 +0100

    NS - get last element into list

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-const items = [];
+let items = [];
 const button = document.getElementById("submit");
 let inputValue = document.getElementById("input");
 const results = document.getElementById("todo-container");
 
-function addTask() {
+button.addEventListener('click', function(){
   const input = inputValue.value;
-  var li = document.createElement("li");
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  li.innerHTML = input;
-  results.appendChild(li);
-  results.appendChild(checkbox);
-}
-
-button.addEventListener("click", addTask);
-
-//on click get input value and push into items array
-
-//for each item create element of span with checkbox
+  
+  // items.push(items[items.length-1])
+  items.push(input)
+  items.forEach((item) => {
+    let p = document.createElement('p');
+    p.innerText = item;
+    results.appendChild(p); 
+  });
+  console.log(items[items.length-1])
+})
