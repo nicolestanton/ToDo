44d54090b47f63f072a0145fe2317fbde7f1b9dd
nicole stanton <nicole.stanton@ao.com>
2020-07-02 09:00:28 +0100

    NS - fixed duplication bug when adding new item

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ const button = document.getElementById("submit");
 let inputValue = document.getElementById("input");
 const results = document.getElementById("todo-container");
 
-button.addEventListener('click', function(){
+button.addEventListener("click", function () {
   const input = inputValue.value;
-  
-  // items.push(items[items.length-1])
-  items.push(input)
+
+  items.push(input);
+  results.innerHTML = "";
   items.forEach((item) => {
-    let p = document.createElement('p');
+    let p = document.createElement("p");
     p.innerText = item;
-    results.appendChild(p); 
+    results.appendChild(p);
   });
-  console.log(items[items.length-1])
-})
+  console.log(items[items.length - 1]);
+});
