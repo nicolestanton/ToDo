05e3128081c4c44a57f8f0597c82e3b73375169d
nicole stanton <nicole.stanton@ao.com>
2020-07-03 16:00:34 +0100

    NS - added delete functionality so it works

--- src/index.js
+++ src/index.js
@@ -6,29 +6,42 @@ const results = document.getElementById("todo-container");
 // TODO: Write tests for this function
 function inputValueIsValid(input) {
   if (input.trim().length === 0 || input.trim() === "") {
-    alert("Enter a valid item");
+    return false;
     // return now so the function doesnt get executed any further
-    return;
   }
+  return true;
 }
 
 // TODO: Write tests for this function
-function createItem(items, el = "p") {
+function createItem(items) {
   items.forEach((item) => {
-    let p = document.createElement(el);
+    let p = document.createElement("p");
     p.innerText = item;
     results.appendChild(p);
+    let deleteItem = document.createElement("span");
+    deleteItem.innerText = "delete";
+    results.appendChild(deleteItem);
+
+    deleteItem.addEventListener("click", function () {
+      results.removeChild(p);
+      results.removeChild(deleteItem);
+      items.pop();
+    });
   });
 }
 
 button.addEventListener("click", function () {
   const input = inputValue.value;
 
-  inputValueIsValid(input);
+  if (!inputValueIsValid(input)) {
+    alert("Enter a valid item");
+    return;
+  }
 
   items.push(input);
 
   results.innerHTML = "";
 
   createItem(items);
+  console.log(items);
 });
